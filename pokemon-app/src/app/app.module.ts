95b02638d335274689cfcd7302f140caf0824dbb
marcusbugge <marcus_bugge@hotmail.com>
2023-03-01 10:33:40 +0100

    pages

--- pokemon-app/src/app/app.module.ts
+++ pokemon-app/src/app/app.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { LoginPage } from './pages/login/login.page';
+import { CataloguePage } from './pages/catalogue/catalogue.page';
+import { CollectionPage } from './pages/collection/collection.page';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPage,
+    CataloguePage,
+    CollectionPage
   ],
   imports: [
     BrowserModule,
