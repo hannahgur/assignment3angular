1287e0f4a83949686ec3009e9f2da92a79d7dd4d
hannahgur <85107636+hannahgur@users.noreply.github.com>
2023-03-03 19:24:02 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,9 @@
 export const environment = {
     production: true,
-    apiTrainers: "",
+    apiTrainers: "https://mkb-noroff-api.herokuapp.com/trainers",
     apiPokemons: "https://pokeapi.co/api/v2/",
 
-    apiKey: ""
+    apiKey: "KvgEBVWGhEimxKi3IuvDyg=="
  
 
 };
