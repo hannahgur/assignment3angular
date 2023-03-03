4a7bf76cdd750612be801720ba9f38a61dcb14ea
Hannah <hannahguren@hotmail.com>
2023-03-03 19:13:35 +0100

    fix: env

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiTrainers: "https://mkb-noroff-api.herokuapp.com/trainers",
+    apiTrainers: "",
     apiPokemons: "https://pokeapi.co/api/v2/",
-    apiKey: "KvgEBVWGhEimxKi3IuvDyg=="
+    apiKey: ""
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,9 @@
 export const environment = {
     production: true,
-    apiTrainers: "https://mkb-noroff-api.herokuapp.com/translations",
+    apiTrainers: "",
     apiPokemons: "https://pokeapi.co/api/v2/",
 
-    apiKey: "KvgEBVWGhEimxKi3IuvDyg=="
+    apiKey: ""
  
 
 };
